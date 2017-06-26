5b3c8546048b5aa2cce6be96c94bde71564361dd
Daniel Thomas <danielt998@gmail.com>
2017-06-26 22:23:33 +0100

    gradle changes

--- src/main/java/TradToSimp/Main.kt
+++ src/main/java/TradToSimp/Main.kt
@@ -1,6 +1,8 @@
+//@file:JvmName("Launcher")
 package TradToSimp
 import DictionaryHandler.Handler
 import DictionaryHandler.Word
+//import kotlin.String
 //import java.util.List
 import java.util.ArrayList
 //import kotlin.collections.ArrayList
@@ -11,29 +13,31 @@ class Main{
     val lines:List<String>? = FileUtils.fileToStringArray(fileName)
     val outputLines:List<String> = ArrayList<String>()
     for(line:String in outputLines){
+//System.out.println(line::class)
+/*
       val builder:StringBuilder = StringBuilder()
-      for(c:Char? in line.toCharArray()){
+      for(c:Char in line.toCharArray()){
         //assume that every individual character exists in dictionary
-        c?.let {
           val word:Word= dictionaryHandler.getWordFromChinese(c)
           if(word!=null){
             builder.append(word)
           }else{
             builder.append(c)
           }
-        }
       }
       outputLines.add(builder.toString())
+*/
     }
   }
-
-  fun main(args:Array<String>){
-    if(args.size!=0){
-      for(fileName:String in args){
-      System.out.println(convertToSimplified(fileName));
-    }
-    }else{
-      //read from standard input
-    }
+  companion object{
+   @JvmStatic fun main(args: Array<String>){
+     if(args.size!=0){
+       for(fileName:String in args){
+         System.out.println(Main().convertToSimplified(fileName));
+       }
+     }else{
+       //read from standard input
+     }
+   }
   }
 }
