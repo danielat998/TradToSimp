1b56e3d3659fb7eb1eac505aa4762ac952c233b1
Daniel Thomas <danielt998@gmail.com>
2017-07-03 01:03:28 +0800

    code works, need to do a)read from standard input and output to file, and b)SimplifiedToTraditional (make this nice and generic somehow)

--- src/main/java/TradToSimp/Main.kt
+++ src/main/java/TradToSimp/Main.kt
@@ -1,33 +1,27 @@
-//@file:JvmName("Launcher")
 package TradToSimp
 import DictionaryHandler.Handler
 import DictionaryHandler.Word
-//import kotlin.String
-//import java.util.List
 import java.util.ArrayList
-//import kotlin.collections.ArrayList
 class Main{
-  fun convertToSimplified(fileName:String){
+  fun convertToSimplified(fileName:String):MutableList<String>{
     val dictionaryHandler:Handler = Handler()
-
-    val lines:List<String>? = FileUtils.fileToStringArray(fileName)
-    val outputLines:List<String> = ArrayList<String>()
-    for(line:String in outputLines){
-//System.out.println(line::class)
-/*
+    val lines:MutableList<String> = FileUtils.fileToStringArray(fileName)
+    val outputLines:MutableList<String> = ArrayList<String>()
+    for(line:String in lines){
       val builder:StringBuilder = StringBuilder()
       for(c:Char in line.toCharArray()){
         //assume that every individual character exists in dictionary
-          val word:Word= dictionaryHandler.getWordFromChinese(c)
-          if(word!=null){
-            builder.append(word)
+          val word:Word?= dictionaryHandler.getWordFromChinese(c)
+
+          if(word!=null){//(if word is not Chinese - seems to be mainly [。，etc])
+            builder.append(word.getSimplifiedChinese())
           }else{
             builder.append(c)
           }
       }
       outputLines.add(builder.toString())
-*/
     }
+    return outputLines
   }
   companion object{
    @JvmStatic fun main(args: Array<String>){
